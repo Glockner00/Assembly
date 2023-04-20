3050c9f6a0aa2eb8cb6673665531d8714e6d1c2a
Axel Glöckner <axel.glockner00@gmail.com>
2023-04-20 08:16:24 +0200

    finish

--- Labb2/AssemblerApplication1/AssemblerApplication1/main.asm
+++ Labb2/AssemblerApplication1/AssemblerApplication1/main.asm
@@ -1,73 +1,114 @@
-INIT :  .def	N = r24			 ; ETT BEEPINTERVALL
+INIT:   .equ	beepln = 20	 ; ETT BEEPINTERVALL
 		ldi     r16,HIGH(RAMEND) ; STACKPOINTER
 		out     SPH,r16
 		ldi     r16,LOW(RAMEND)
-		out     SPL,r16			 ; INPUT
-		ldi		r16, 0x00
-        out		DDRA, r16
-		ldi     r16,0x8F         ; OUTPUT 
+		out     SPL,r16			 
+		ldi     r16,0x01         ; OUTPUT 
 		out		DDRB,r16
-
 
 MORSE:	ldi		ZL, LOW(MESSAGE*2)  ; load the message, low (top of the stack)
 		ldi		ZH, HIGH(MESSAGE*2) ; load the message, high (top of the stack)
-		rcall	GET_CHAR			; get the next ASCII 
+		rcall	GET_CHAR
+
 
 ; MAIN LOOP FOR SENDING THE MESSAGE.
-ONE_CHAR:	cpi		r18, 0x20	; compare with a space
-			breq	SPACE		; branch if there is a space
-			rcall	LOOKUP		; else go to lookup and translate the ASCII character to binary.
-			rcall	SEND		; then send 
+ONE_CHAR:	
+	cpi	r18, 0x20	; compare with a space
+	breq SPACE		; branch if there is a space
+	rcall LOOKUP		; else go to lookup and translate the ASCII character to binary.
+	rcall SEND
+	call GET_CHAR
+	call NO_BEEP
+	call NO_BEEP
+	cpi r18, 0x00
+	breq END
+	rjmp ONE_CHAR
 
-NO_CHAR:	rcall GET_CHAR ; look for an unknown character
-			cpi r18, 0x00  ; compare wiht zero
-			brne ONE_CHAR  ;
-			rjmp MORSE	   ; 
 
-SPACE:	ldi		r21, 0x07  ; load a blank
-		ldi		r18, 0x20  ; load a space
-		rcall	NO_BEEP	   ; call for a no beep
-		rjmp	NO_CHAR	   ; jump to a non character
+END:
+	RJMP END
 
-; TRANSLATE ASCII TO BINARY
-LOOKUP:		push	ZL
-			push	ZH
-			ldi		ZH, HIGH(BTAB*2)
-			ldi		ZL, LOW(BTAB*2)
-			subi	r18, 0x40
+LOOKUP:
+	push ZL
+	push ZH 
+	ldi ZH, HIGH(BTAB*2)
+	ldi ZL, LOW(BTAB*2)
+	subi r18, 0x41
+	ldi r22, 0
+	add ZL, r18
+	adc ZH, r22
+	lpm r19, Z
+	pop Zh
+	pop Zl
+	ret
 
-; CONTINUE TO CHECK REGISTERS.
-CONTINUE:	ldi r22, 1
-			add ZL, r18
-			brcc pc+2 ; is used to skip the next instruction (i.e., add ZH, r22) if the carry flag is not set
-			add ZH, r22
-			lpm r19, Z
-			pop ZH
-			pop ZL
-			ret 
+SEND:
+	rcall GETNEXTBIT
+	breq SEND_EXIT
+	brcs LONG_BEEP
 
-SEND:	
+SHORT_BEEP:
+	ldi r22, beepln
+	jmp BEEPLOOP
 
-CHAR_DONE:
-
-GET_CHAR:	lpm		r18, Z+
-			ret
-
-GET_BIT:
-
-BEEP:
-
+LONG_BEEP: 
+	ldi r22, beepln*3
 BEEPLOOP:
+	rcall CYCLE
+	dec r22
+	brne BEEPLOOP
+	rcall NO_BEEP_CYCLE
+	jmp SEND
 
-NO_BEEP:
+SEND_EXIT:
+	ret
 
-CYCLE: 
+NO_BEEP: ; En ingen beep
+	ldi r22, beepln
+NOBEEPLOOP:
+	rcall NO_BEEP_CYCLE
+	dec r22
+	brne NOBEEPLOOP
+	rcall DELAY
 
+NO_BEEP_EXIT:
+	ret
+
+SPACE:
+	rcall NO_BEEP
+	rcall NO_BEEP
+	rcall NO_BEEP
+	rcall NO_BEEP
+	rcall NO_BEEP
+	rcall NO_BEEP
+	rcall NO_BEEP
+	rcall GET_CHAR
+	jmp ONE_CHAR
+
+GET_CHAR:
+	lpm r18, Z+
+	ret
+
+GETNEXTBIT:
+	lsl r19
+	ret	
+	
+CYCLE:
+	sbi PORTB, 0
+	rcall DELAY
+	cbi PORTB, 0
+	rcall DELAY
+	ret
+
+NO_BEEP_CYCLE:
+	rcall DELAY
+	rcall DELAY
+	ret
 
 DELAY:
-            ldi		r16,0x02
+            ldi		r16, 30
 delayYttreLoop:
-            ldi		r17,0x22
+            ldi		r17, 40
 delayInreLoop:
             dec		r17
             brne	delayInreLoop
@@ -76,7 +117,9 @@ delayInreLoop:
             ret
            
 .org $200
-MESSAGE:	.db "SOS",$00
+MESSAGE:	.db "SOS HEJ",$00
  
 .org $230
-BTAB:	.db $00, $60, $88, $A8, $90, $40, $28, $D0, $08, $20, $78, $B0, $48, $E0, $A0, $F0, $68, $D8, $50, $10, $C0, $30, $18, $70, $98, $B8, $C8
+BTAB:	.db  $60, $88, $A8, $90, $40, $28, $D0, $08, $20, $78, $B0, $48, $E0, $A0, $F0, $68, $D8, $50, $10, $C0, $30, $18, $70, $98, $B8, $C8
+
+
